eb047ff27bbdfd498f313df95840eb6db1e3a547
unknown <eltad2003@gmail.com>
2025-05-26 01:13:23 +0700

    update key captcha

--- frontend/src/baseUrl.js
+++ frontend/src/baseUrl.js
@@ -3,4 +3,4 @@ export const urlBE = `https://${url}`
 export const urlChat = 'http://localhost:8000'
 export const urlSocket = `wss://${url}/ws/orders`
 export const keyCaptchaLocalhost = '6LehoyorAAAAAPD3Z0LvRCpIvQNSw3gRztEzvGFz'
-export const keyCaptcha = '6Ld-q0grAAAAABPgwQdGVqaamXFxbeb0kDieOLgI'
+export const keyCaptcha = '6LeY3UgrAAAAAH0OnOE-hDn7KXqsL6hPv1u6gtlN'
